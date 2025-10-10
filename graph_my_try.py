e6949af4b4dbacc919b20ebcf7c4eee632baa569
svobodn1y <sovailya2208@gmail.com>
2025-10-10 22:43:31 +0300

    remove/get _neighbor

--- graph_my_try.py
+++ graph_my_try.py
@@ -10,3 +10,14 @@ class Node:
 
         if neighbor not in self.neighbors:
             self.neighbors[neighbor] = weight
+
+    def remove_neighbor(self, neighbor):
+
+        if neighbor in self.neighbors:
+            del self.neighbors[neighbor]
+
+    def get_neighbors(self):
+
+        return list(self.neighbors.keys())
+
+    def get_weight_to_neighbor(self, neighbor):
