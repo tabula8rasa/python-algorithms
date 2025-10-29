2951af207a4745d8b508e59f908cc0af3fe94d18
svobodn1y <sovailya2208@gmail.com>
2025-10-29 14:49:00 +0300

    v1.3

--- test.py
+++ test.py
@@ -1,16 +1,14 @@
-from itertools import batched
+
 import pyglet
-from fontTools.merge.util import current_time
+
 from pyglet.window import key
 from pyglet import shapes
-from math import cos, sin, pi
-from typing import List, Tuple
-import random
+
 import datetime
-import time
+
 
 # Create a window
-window = pyglet.window.Window(width=1920, height=1080, caption='Pyglet Example')
+window = pyglet.window.Window(width=1750, height=500,caption='Pyglet Example')
 
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
@@ -22,21 +20,22 @@ SYMBOL = None
 class Clock:
 
     def __init__(self, origin_x: int, origin_y: int, radius: int):
-        self.circle = shapes.Circle(x=origin_x, y=origin_y, segments = 500, radius=radius, color=(250, 250, 250), batch=CLOCK)
-        self.arrow_hour = shapes.Line(x=origin_x, y=origin_y, x2=origin_x + radius, y2=origin_y, thickness=4.8, color=(240, 20, 20), batch=CLOCK)
-        self.arrow_min = shapes.Line(x=origin_x, y=origin_y, x2=origin_x + radius, y2=origin_y, thickness=4.8, color=(20, 240, 20), batch=CLOCK)
+
+        self.border_circle = shapes.Circle(x=origin_x, y=origin_y, segments = 500, radius=radius+5, color=(168, 168, 168), batch=CLOCK)
+        self.circle = shapes.Circle(x=origin_x, y=origin_y, segments=500, radius=radius, color=(250, 250, 250), batch=CLOCK)
+        self.arrow_hour = shapes.Line(x=origin_x, y=origin_y, x2=origin_x + radius, y2=origin_y, thickness=6, color=(0,0, 0), batch=CLOCK)
+        self.arrow_min = shapes.Line(x=origin_x, y=origin_y, x2=origin_x + radius, y2=origin_y, thickness=6, color=(0, 0, 0), batch=CLOCK)
         self.states = {'H':[0, 180],
                        'V':[270,90],
-                       'BL':[180,270],
-                       'BR':[0,270],
-                       'TL':[180,90],
-                       'TR':[0,90],
+                       'TL':[180,270],
+                       'TR':[0,270],
+                       'BL':[180,90],
+                       'BR':[0,90],
                        'E':[135,135]}
         self.position = 'E'
         self.arrow_hour.rotation = self.states[self.position][0]
         self.arrow_min.rotation = self.states[self.position][1]
 
-#obj = Clock(origin_x=500, origin_y=500, radius=150)
 
 digits = [
   [
@@ -122,15 +121,9 @@ digits = [
 
 t = datetime.datetime.now()
 delta = datetime.timedelta(seconds=1/60)
-print(t.hour)
-print(t.minute)
-print(t.second)
-
 
 def wtfamidoing(obj, state):
-    # global SYMBOL
-    # if SYMBOL is not None:
-    #     state = chr(SYMBOL).upper()
+
     if obj.arrow_hour.rotation % 360  != obj.states[state][0] or obj.arrow_min.rotation % 360 != obj.states[state][1]:
         h, m = 0, 0
         if obj.arrow_hour.rotation % 360 == obj.states[state][0] and h == 0:
@@ -147,12 +140,12 @@ def wtfamidoing(obj, state):
             SYMBOL = None
 
 # objs = [Clock(origin_x=200 + 55 * i, origin_y=100 + 55 * j,  radius = 25) for j in range(6) for i in range(24)]
-h_f_digit = [Clock(origin_x=200 + 55 * i, origin_y=100 + 55 * j,  radius = 25) for j in range(6) for i in range(4)]
-h_s_digit = [Clock(origin_x=210 + 55 * i, origin_y=100 + 55 * j,  radius = 25) for j in range(6) for i in range(4,8)]
-m_f_digit = [Clock(origin_x=240 + 55 * i, origin_y=100 + 55 * j,  radius = 25) for j in range(6) for i in range(8,12)]
-m_s_digit = [Clock(origin_x=250 + 55 * i, origin_y=100 + 55 * j,  radius = 25) for j in range(6) for i in range(12,16)]
-s_f_digit = [Clock(origin_x=280 + 55 * i, origin_y=100 + 55 * j,  radius = 25) for j in range(6) for i in range(16,20)]
-s_s_digit = [Clock(origin_x=290 + 55 * i, origin_y=100 + 55 * j,  radius = 25) for j in range(6) for i in range(20,24)]
+h_f_digit = [Clock(origin_x=200 + 55 * i, origin_y=375 - 55 * j,  radius = 25) for j in range(6) for i in range(4)]
+h_s_digit = [Clock(origin_x=210 + 55 * i, origin_y=375 - 55 * j,  radius = 25) for j in range(6) for i in range(4,8)]
+m_f_digit = [Clock(origin_x=240 + 55 * i, origin_y=375 - 55 * j,  radius = 25) for j in range(6) for i in range(8,12)]
+m_s_digit = [Clock(origin_x=250 + 55 * i, origin_y=375 - 55 * j,  radius = 25) for j in range(6) for i in range(12,16)]
+s_f_digit = [Clock(origin_x=280 + 55 * i, origin_y=375 - 55 * j,  radius = 25) for j in range(6) for i in range(16,20)]
+s_s_digit = [Clock(origin_x=290 + 55 * i, origin_y=375 - 55 * j,  radius = 25) for j in range(6) for i in range(20,24)]
 objs =[h_f_digit,h_s_digit,m_f_digit,m_s_digit,s_f_digit,s_s_digit]
 
 def update(dt):
@@ -178,10 +171,13 @@ def update(dt):
         second_f = str(t.second)[0]
         second_s = str(t.second)[1]
 
-    print(second_f)
-    # second_f = '0'
+    # print(second_f)
     i=0
     j=0
+    ii=0
+    jj=0
+    iii=0
+    jjj=0
 
     for clock in objs[-1]:
         wtfamidoing(clock, digits[int(second_s)][i])
@@ -189,6 +185,18 @@ def update(dt):
     for clock in objs[-2]:
         wtfamidoing(clock, digits[int(second_f)][j])
         j += 1
+    for clock in objs[-3]:
+        wtfamidoing(clock, digits[int(minute_s)][ii])
+        ii += 1
+    for clock in objs[-4]:
+        wtfamidoing(clock, digits[int(minute_f)][jj])
+        jj += 1
+    for clock in objs[-5]:
+        wtfamidoing(clock, digits[int(hour_s)][iii])
+        iii += 1
+    for clock in objs[-6]:
+        wtfamidoing(clock, digits[int(hour_f)][jjj])
+        jjj += 1
 
 
 pyglet.clock.schedule_interval(update, 1/60)
